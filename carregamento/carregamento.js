4f6c159b38b4a877f480529cb094aeb21893281f
Mateus Oliveira <mateusoliveiracruz4@gmail.com>
2025-05-27 13:02:55 -0300

    Add: Tela de Loading, Gravuras e removido bordas da versão mobile

---
+++ carregamento/carregamento.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        document.getElementById("carreg").classList.add("hidden");
+    }, 2000); // Adiciona um atraso de 5 segundos
+});
+
